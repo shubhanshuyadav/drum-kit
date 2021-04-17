0f37804f71f3aee7af9d1054a0cb11de0c2a36ff
shubhanshuyadav <shubhanshuemailid@gmail.com>
2021-04-17 12:44:05 +0530

    final

--- index.js
+++ index.js
@@ -3,19 +3,28 @@
 $(".drum").on("click",function(){
     var buttonInnerHTML=this.innerHTML;
     makeSound(buttonInnerHTML);
+    animation(buttonInnerHTML);
 })
 
 
 //maneging keypress
 
-$(".drum").on("keydown",function(){
-    var buttonInnerHTML=this.key;
-    makeSound(buttonInnerHTML);
-})
+$("*").on("keydown",function(event){
+    makeSound(event.key);
+    animation(event.key);
+});
 
 
 // Animation to button on any event
-function animation(){}
+function animation(currentKey){
+var currentButton=$("."+currentKey);
+currentButton.addClass("pressed");
+
+setTimeout(function(){
+    currentButton.removeClass("pressed"); 
+},100);
+
+}
 
 
 function makeSound(key){
@@ -23,37 +32,30 @@ function makeSound(key){
     switch (key) {
         case "w":   var tom1 = new Audio('sounds/tom-1.mp3');
                     tom1.play();
-                    console.log("Audio played");
                     break;
 
         case "a":   var tom2 = new Audio('sounds/tom-2.mp3');
                     tom2.play();
-                    console.log("Audio played");
                     break;
 
         case "s":   var tom3 = new Audio('sounds/tom-3.mp3');
                     tom3.play();
-                    console.log("Audio played");
                     break;
 
         case "d":   var tom4 = new Audio('sounds/tom-4.mp3');
                     tom4.play();
-                    console.log("Audio played");
                     break;
 
         case "j":   var crash = new Audio('sounds/crash.mp3');
                     crash.play();
-                    console.log("Audio played");
                     break;
 
         case "w":   var snare = new Audio('sounds/snare.mp3');
                     snare.play();
-                    console.log("Audio played");
                     break;
 
         case "w":   var kick = new Audio('sounds/kick.mp3');
                     kick.play();
-                    console.log("Audio played");
                     break;
     
         default:    console.log("default click");
